716385bb4f83617c01bc0e923b4987ab1b202341
81angelHELP98 <vangel338@gmail.com>
2024-03-09 11:33:11 -0300

    agrego nodemon y conf del servidor local

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log("Run..")
+const express = require("express");
+const app = express();
+
+//Setter port
+app.set("PORT", process.env.PORT || 6666);
+
+//Middelwares
+app.use(express.json());
+//Format data:
+app.use(express.urlencoded({extended: true}));
+
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+let number = null
+
+console.log(typeof number)
+
+app.listen(app.get("port"), () => { console.log("ecommerce-backend run in port ", app.get("PORT")) });
